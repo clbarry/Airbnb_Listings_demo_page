30da6925449034847ddeb5837b4c045e611473c3
clbarry <c.barry@northeastern.edu>
2026-06-10 09:27:51 -0400

    Update Listings.js

--- js/Listings.js
+++ js/Listings.js
@@ -1,9 +1,18 @@
 export default function Listings() {
   const Listings = {};
 
+  /* Initialize Listings - From the HTML create-listings-form */
   console.log("👍🏼 Listings Initialized");
   const form = document.getElementById("create-listing-form");
 
+    const listingsContainer = document.querySelector("#listings-container");
+
+  /* append listing to the page */
+  const appendListing = (listing) => {
+    console.log("Append Listing", listing);
+  };
+
+  /* event handler for creating a listing */
   const onCreate = (event) => {
     event.preventDefault();
     console.log("🤓 On create listing", event);
@@ -13,6 +22,8 @@ export default function Listings() {
   };
 
   form.addEventListener("submit", onCreate);
+
+  appendListing(listing);
 
   return Listings;
 }
