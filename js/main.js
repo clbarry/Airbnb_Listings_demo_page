4039bfbc367ece01f7a66b7d8a52a3bf1c0bd8e6
clbarry <c.barry@northeastern.edu>
2026-06-09 22:12:47 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-/*import Listings from "./js/Listings"
+/* initialize modules */
+import Listings from "./Listings.js"
 
 /* define constants */
 const inTitle = document.querySelector("#input-title")
@@ -21,3 +22,30 @@ const onClickCancel = (event) => {
 
 btnCancel.addEventListener("click", onClickCancel);
 
+
+
+function MaineModule(name ="Main Module") {
+    const me = {};
+
+    console.log(`${name} initialized`)
+
+    let count = 0
+
+    function incr() {
+        count += 1;
+        return count;
+    }
+
+    me.incr = incr;
+    me.name=name;
+    return me;
+}
+
+/* main */
+const main= MaineModule();
+const secondary = MaineModule("Secondary Module");
+
+console.log("main incr", main.incr());
+console.log("main incr", main.incr());
+console.log('secondary incr', secondary.incr());
+console.log("main incr", main.incr());
