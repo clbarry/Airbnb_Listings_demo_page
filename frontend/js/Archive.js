89ef9d209dd96409d2d355a70d0d2ec5bc02038a
clbarry <c.barry@northeastern.edu>
2026-06-14 22:29:13 -0400

    created frontend file

---
+++ frontend/js/Archive.js
@@ -0,0 +1,53 @@
+/* ARCHIVE FOR MY USE */
+/* Used to practice */
+/* initialize modules */
+import Listings from "./Listings.js"
+
+/* define constants */
+const inTitle = document.querySelector("#input-title")
+const inPrice = document.querySelector("#input-price")
+const inDescription = document.querySelector("#input-description")
+const btnCancel = document.querySelector("#btn-cancel")
+
+
+
+/* event handlers */
+
+/* cancel button */
+const onClickCancel = (event) => {
+    /* don't navigate to another page */
+    event.preventDefault();
+    inTitle.value = "";
+    inPrice.value = "200";
+    inDescription.value = "";
+}    
+
+btnCancel.addEventListener("click", onClickCancel);
+
+
+
+function MaineModule(name ="Main Module") {
+    const me = {};
+
+    console.log(`${name} initialized`)
+
+    let count = 0
+
+    function incr() {
+        count += 1;
+        return count;
+    }
+
+    me.incr = incr;
+    me.name=name;
+    return me;
+}
+
+/* main */
+const main= MaineModule();
+const secondary = MaineModule("Secondary Module");
+
+console.log("main incr", main.incr());
+console.log("main incr", main.incr());
+console.log('secondary incr', secondary.incr());
+console.log("main incr", main.incr());
