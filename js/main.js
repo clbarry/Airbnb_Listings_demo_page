c5b9a185f65db62904ad81a2cf4bd3a392ec5c17
clbarry <c.barry@northeastern.edu>
2026-06-14 22:17:17 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,51 +1,13 @@
 /* initialize modules */
 import Listings from "./Listings.js"
 
-/* define constants */
-const inTitle = document.querySelector("#input-title")
-const inPrice = document.querySelector("#input-price")
-const inDescription = document.querySelector("#input-description")
-const btnCancel = document.querySelector("#btn-cancel")
-
-
-
-/* event handlers */
-
-/* cancel button */
-const onClickCancel = (event) => {
-    /* don't navigate to another page */
-    event.preventDefault();
-    inTitle.value = "";
-    inPrice.value = "200";
-    inDescription.value = "";
-}    
-
-btnCancel.addEventListener("click", onClickCancel);
-
-
-
-function MaineModule(name ="Main Module") {
+function MainModule() {
     const me = {};
 
-    console.log(`${name} initialized`)
+    me.listings = Listings();
 
-    let count = 0
-
-    function incr() {
-        count += 1;
-        return count;
-    }
-
-    me.incr = incr;
-    me.name=name;
     return me;
 }
 
 /* main */
-const main= MaineModule();
-const secondary = MaineModule("Secondary Module");
-
-console.log("main incr", main.incr());
-console.log("main incr", main.incr());
-console.log('secondary incr', secondary.incr());
-console.log("main incr", main.incr());
+const main = MainModule();
